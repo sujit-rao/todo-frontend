7dcd2d97404b5c7186ff811ba7a238dc4130e3e2
Sujit Rao <sujitrao867@gmail.com>
2024-04-19 22:46:40 +0530

    exp with the bckend link

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get(`https://todo-backend-orpin-psi.vercel.app/api/get`)
+      .get(`https://todo-backend-orpin-psi.vercel.app/get`)
       .then((res) => setTodos(res.data))
       .catch((err) => console.log(err))
   }, [updateUI]);
@@ -22,7 +22,7 @@ function App() {
   const saveToDO = () => {
 
     axios
-      .post(`https://todo-backend-orpin-psi.vercel.app/api/save`, { toDo: input })
+      .post(`https://todo-backend-orpin-psi.vercel.app/save`, { toDo: input })
       .then((res) => {
         console.log(res.data);
         setInput("")
